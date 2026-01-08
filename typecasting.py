8a63b41581135dd534520c1161d6fa5f9c13f4fc
killertntcodes <killertnt.playz@gmail.com>
2026-01-08 17:54:26 +0400

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+name="arnav"
+age=14
+is_student=True
+weight=52.75
+
+print("Name:",name)
+print("the data type of Name is:",type(name))
+print("age:",age)
+print("the data type of age is:",type(age))
+print("is_student:",is_student)
+print("the data type of is_student is:",type(is_student))
+print("weight:",weight)
+print("the data type of Weight is:",type(weight))
+
+age=str(age)
+print(age)
+weight=int(weight)
+print(weight)
