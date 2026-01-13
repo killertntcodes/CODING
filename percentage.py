1fd58c544de2cfa6bbd2f287c4d48a15f9c867da
killertntcodes <killertnt.playz@gmail.com>
2026-01-13 18:53:49 +0400

    percentage marks

---
+++ percentage.py
@@ -0,0 +1,9 @@
+math= int(input("math:"))
+english= int(input("english:"))
+science= int(input("science:"))
+hindi= int(input("hindi:"))
+sum = math+english+science+hindi
+print("sum of math, english, science and hindi")
+perc = (sum/400)*100
+print(end="percentage mark =")
+print(perc)
