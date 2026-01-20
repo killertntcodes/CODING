2f26b16568714074550f222deddcea6a05b28378
killertntcodes <killertnt.playz@gmail.com>
2026-01-20 17:24:22 +0400

    not-equal

---
+++ not-equal-to-operator.py
@@ -0,0 +1,21 @@
+x= 10
+y= 12
+z= 12
+print(x != y)  
+print(y != z)
+
+a= "python"
+b= "coding"
+
+if a != b:
+    print(a, "and ", b, "are different")
+
+w=4
+s=5
+
+if (w==1) != (s==5):
+    print("hello")
+
+d= int(input("enter a number:"))
+if d%2 != 0:
+    print(d, "is odd")
