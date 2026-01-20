35d651332160a5b0a6072fc3efef852f2916956e
killertntcodes <killertnt.playz@gmail.com>
2026-01-20 17:14:18 +0400

    and-or

---
+++ AND-OR.py
@@ -0,0 +1,17 @@
+x=10
+y=12
+z=0
+if x and y and z:
+    print("all the numbers are boolean")
+else:
+    print("at least one of the numbers is not boolean")
+
+a=10
+b=-12
+c=0
+if a>0 or c>0:
+    print("at least one of the numbers is bigger than 0")
+elif a>0 and b>0:
+    print("both numbers are bigger than 0")
+else:
+    print("none of the numbers is bigger than 0")
