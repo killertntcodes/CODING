553f88a132a207e2cd4cf9245e22f27fd6d48542
killertntcodes <killertnt.playz@gmail.com>
2026-01-29 17:27:10 +0400

    divisibility

---
+++ divisibility.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number: "))
+y= int(input("Enter another number: "))
+if x % y == 0:
+    print(f"{x} is divisible by {y}")  
+else:
+    print(f"{x} is not divisible by {y}")
