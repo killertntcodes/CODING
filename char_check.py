8480ce7093a8783d1c1ac9941d2d9860361b091d
killertntcodes <killertnt.playz@gmail.com>
2026-01-30 18:08:15 +0400

    char check

---
+++ char_check.py
@@ -0,0 +1,7 @@
+x= input("enter a character: ")
+if x.isalpha():
+    print("The character is an alphabet")
+elif x.isdigit():
+    print("The character is a digit")
+else:
+    print("The character is a special character")
