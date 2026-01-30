a13515d57cb085b766cb830a2e0b3fe99d4354a7
killertntcodes <killertnt.playz@gmail.com>
2026-01-30 17:59:56 +0400

    rides

---
+++ rides.py
@@ -0,0 +1,13 @@
+x= input("what do u prefer? car or bike?: ")
+if x=="car":
+    input("sedan or suv?: ")
+    if x=="sedan":
+        print("You have chosen a sedan car")
+    else:
+        print("You have chosen an suv car")
+else:
+    input("motrocycle or scooter?: ")
+    if x=="motrocycle":
+        print("You have chosen a motorcycle")
+    else:
+        print("You have chosen a scooter")
