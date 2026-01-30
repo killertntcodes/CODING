8018817f7a4dd3f1b09eb6e820246cc1d8c35108
killertntcodes <killertnt.playz@gmail.com>
2026-01-30 17:55:42 +0400

    bill

--- consuming.py
+++ consuming.py
@@ -1,9 +1,9 @@
 x=int(input("enter how many units u consumed: "))
 if x<50:
-    print("bill=27.6")
+    print("the bill is : ", (x*2.60)+25)
 elif x<100 and x>=50:
-    print("bill=38.25")
+    print("the bill is : ", (x*3.25)+35)
 elif x<200 and x>=100:
-    print("bill=50.26")
+    print("the bill is : ", (x*5.26)+45)
 else:
-    print("bill=83.45")
+    print("the bill is : ", (x*8.45)+75)
