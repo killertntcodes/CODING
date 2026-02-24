72b8eaa9b373b9cae0b43a76d446b0726de6a329
killertntcodes <killertnt.playz@gmail.com>
2026-02-24 17:30:13 +0400

    factorial

---
+++ factorial.py
@@ -0,0 +1,7 @@
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
+x = int(input("enter a number: "))
+print("the factorial of the number is: ", factorial(x))
