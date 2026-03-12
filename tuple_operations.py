1d535a518690db0843fbf7b98d039793fcb26b2e
killertntcodes <killertnt.playz@gmail.com>
2026-03-12 16:47:20 +0400

    Tuple operations

---
+++ tuple_operations.py
@@ -0,0 +1,18 @@
+x=("tuple", False, 3.14, 6)
+print(x)
+
+x=(4, 5, 6, 8, 3, 2)
+print(x)
+
+x=x+(9,)
+print(x)
+
+x=(50, 40, 60, 50, 80)
+print(x.count(50))
+
+x=(2,4,3,5,6,1,5,8,9,0)
+_slice=x[3:5]
+print(_slice)
+
+_slice=x[:6]
+print(_slice)
