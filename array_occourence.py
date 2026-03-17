e4363e63adeb861b9017236fda8951f47c13fef2
killertntcodes <killertnt.playz@gmail.com>
2026-03-17 17:20:14 +0400

    array occourences

---
+++ array_occourence.py
@@ -0,0 +1,4 @@
+x=eval(input("enter an array: "))
+y=eval(input("enter a value to check the occurrence of: "))
+print("the number of times ",y," appears in the array is: ",x.count(y))
+print("the array in reverse is: ",x[::-1])
