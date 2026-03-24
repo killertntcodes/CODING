3b4d325697093055ddece1374fa0f22bc13e4e9e
killertntcodes <killertnt.playz@gmail.com>
2026-03-24 16:48:30 +0400

    clas vehical

---
+++ vehical_clas.py
@@ -0,0 +1,7 @@
+class vehical:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+modelX = vehical(240, 18)
+print("model max speed is ", modelX.max_speed)
+print("model mileage is ", modelX.mileage)
