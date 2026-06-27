1a2039aed7fc66032e44767fc722371380981692
killertntcodes <killertnt.playz@gmail.com>
2026-06-27 15:37:19 +0400

    color convos

---
+++ color_conversations_n_cropping.py
@@ -0,0 +1,15 @@
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread('colorful image.jpg')
+
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+plt.imshow(image_rgb, cmap='gray')
+plt.title('Grayscale Image')
+plt.show()
+
+cropped_image = image_rgb[100:300, 200:400]
+cropped_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+plt.imshow(cropped_rgb)
+plt.title('Cropped Image')
+plt.show()
